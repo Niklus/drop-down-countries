b4c8bca5b8ebc99bb80317b9360c4efdb8732227
Niklus <niklasoti@gmail.com>
2021-05-24 11:51:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# drop-down-countries
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/drop-down-countries)
---
+++ index.js
@@ -0,0 +1,27 @@
+// Import stylesheets
+import './style.css';
+
+const select = document.querySelector("#countries-dropdown");
+
+const countries = [
+  "Kenya", 
+  "Tanzania", 
+  "Uganda", 
+  "Rwanda", 
+  "Burundi", 
+  "Sudan"
+];
+
+function getDropdown(countries) {
+  let htmlStr = '<option value="">Please select</option>';
+  countries.forEach(country => {
+    htmlStr += `<option value=${country.toLowerCase()}>${country}</option>`;
+  });
+  return htmlStr;
+}
+
+select.innerHTML = getDropdown(countries);
+
+select.addEventListener('input', e => {
+  document.getElementById("country").innerText = e.target.value.toUpperCase();
+})
